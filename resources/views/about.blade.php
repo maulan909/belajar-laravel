e5bf84a6e5905c7f10f995870bef05a92c2e8088
maulan909 <maulanahilmi909@gmail.com>
2021-10-15 14:53:28 +0700

    membuat route dan view home, about, blog

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hilmi Blog | Halaman {{ $title }}</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+
+<body>
+    <h1>Halaman {{ $title }}</h1>
+    <p>{{ $name }}</p>
+    <p>{{ $job }}</p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        'title' => 'Home'
+    ]);
+});
+
+Route::get('about', function () {
+    return view('about', [
+        'title' => 'About',
+        'name' => 'Hilmi Maulana Mukhlis',
+        'job' => 'Programmer'
+    ]);
+});
+
+Route::get('blog', function () {
+    return view('blog', [
+        'title' => 'Blog'
+    ]);
 });
